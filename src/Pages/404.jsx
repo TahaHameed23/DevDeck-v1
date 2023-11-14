4bb9210e64609631bb87db5378ecdefee433e371
TahaHameed23 <89855847+TahaHameed23@users.noreply.github.com>
2023-11-15 00:08:14 +0530

    style:404 page

--- src/Pages/404.jsx
+++ src/Pages/404.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 function ERR_404() {
   return (
-    <div className='selection:bg-blue-400 flex flex-col gap-y-8 items-center h-screen justify-center bg-slate-900'>
+    <div className=' selection:bg-blue-400 flex flex-col gap-y-8 items-center h-screen justify-center bg-slate-900'>
 
     <h1 className='text-9xl font-bold text-slate-200'>404</h1>
-    <h2 className='text-4xl font-medium text-slate-300' >You have found a secret place.</h2>
-    <p className='text-lg text-slate-400 text-center'>Unfortunately, this is only a 404 page. You may have mistyped <br /> the address, or the page has been moved to another URL.</p>
+    <h2 className=' text-center text-2xl md:text-3xl lg:text-4xl font-medium text-slate-300' >You have found a secret place.</h2>
+    <p className='text-lg md:text-xl lg:text-xl text-slate-400 text-center mx-4'>Unfortunately, this is only a 404 page. You may have mistyped the address, or the page has been moved to another URL.</p>
     <button className='text-blue-400 p-4 rounded-xl duration-200 text-lg hover:bg-blue-500 hover:bg-opacity-30'>
         <a href="/">Take me back to home page</a>
     </button>
